9bd18a45eca258f5df9adbb81d58390f159bd35f
mustafaTe <mustafatekd@hotmail.com>
2021-05-15 17:30:53 +0300

    End of Section 5

---
+++ Application/Activities/List.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using Domain;
+using MediatR;
+
+namespace Application.Activities
+{
+    public class List
+    {
+        
+        public class Query : IRequest <List<Activity>>  {}  
+    }
+}
