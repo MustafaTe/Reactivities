39ae93660fe6c4bd2da8d4987a532407b1fb6691
mustafaTe <mustafatekd@hotmail.com>
2021-05-29 12:24:06 +0300

    End of Section 12

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -5,9 +5,11 @@ using Domain;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading;
 using Application.Activities;
+using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers
 {
+    [AllowAnonymous]
     public class ActivitiesController : BaseApiController
     {
         [HttpGet]
